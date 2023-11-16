b2bcb4bc3aed9e1cb2e5eeaca9de336da3eb2b38
Silvian Ranetescu <silvian.ranetescu@gmail.com>
2023-11-16 17:57:56 +0200

    hw04 done

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,18 +1,28 @@
 const express = require("express");
-
+const { auth } = require("../../middlewares/auth");
 const router = express.Router();
 const controller = require("../../controllers/controllers");
 
-router.get("/", controller.get);
+router.get("/", auth, controller.get);
 
-router.get("/:contactId", controller.getById);
+router.get("/:contactId", auth, controller.getById);
 
-router.post("/", controller.add);
+router.post("/", auth, controller.add);
 
-router.delete("/:contactId", controller.remove);
+router.delete("/:contactId", auth, controller.remove);
 
-router.put("/:contactId", controller.update);
+router.put("/:contactId", auth, controller.update);
 
-router.patch("/:contactId/favorite", controller.updateStatus);
+router.patch("/:contactId/favorite", auth, controller.updateStatus);
+
+router.post("/users/signup", controller.createAccount);
+
+router.post("/users/login", controller.loginAccount);
+
+router.get("/users/logout", controller.logoutAccount);
+
+router.get("/users/current", controller.getAccount);
+
+router.patch("/users", auth, controller.updateSubscription);
 
 module.exports = router;
--- services/index.js
+++ services/index.js
@@ -1,23 +1,63 @@
-const User = require("./schemas/contactSchema.js");
+const Contact = require("./schemas/contactSchema.js");
+const User = require("./schemas/usersSchema.js");
 
-const getAllContacts = async () => {
-  return User.find();
+const getAllContacts = async (page, limit, favorite) => {
+  const options = {
+    page: page || 1,
+    limit: limit || 20,
+  };
+  const query = favorite ? { favorite: true } : {};
+
+  const result = await Contact.paginate(query, options);
+
+  return result;
 };
 const getContactById = async (id) => {
-  return User.findById(id);
+  return Contact.findById(id);
 };
 const createContact = async (contact) => {
-  return User.create({ ...contact });
+  return Contact.create({ ...contact });
 };
 const deleteContact = async (id) => {
-  return User.findByIdAndDelete(id);
+  return Contact.findByIdAndDelete(id);
 };
 const updateContact = async (id, data) => {
-  return User.findByIdAndUpdate(id, data, { new: true });
+  return Contact.findByIdAndUpdate(id, data, { new: true });
 };
 const updateContactStatus = async (id, data) => {
-  return User.findByIdAndUpdate(id, data, { new: true });
+  return Contact.findByIdAndUpdate(id, data, { new: true });
 };
+const createUser = async ({ email, password }) => {
+  try {
+    const newUser = new User({ email, password });
+    newUser.setPassword(password);
+
+    return await newUser.save();
+  } catch (error) {
+    throw error;
+  }
+};
+const loginUser = async ({ email, password, token }) => {
+  try {
+    const user = await User.findOne({ email });
+
+    if (!user || !user.validPassword(password)) {
+      throw new Error("Wrong email or password");
+    }
+
+    user.setToken(token);
+    await user.save();
+    return user;
+  } catch (error) {
+    throw error;
+  }
+};
+
+const findAccount = async (user) => {
+  const result = await User.findOne({ email: user.email });
+  return result;
+};
+
 module.exports = {
   getAllContacts,
   getContactById,
@@ -25,4 +65,7 @@ module.exports = {
   deleteContact,
   updateContact,
   updateContactStatus,
+  createUser,
+  loginUser,
+  findAccount,
 };
